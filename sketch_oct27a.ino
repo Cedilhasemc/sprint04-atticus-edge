a2622e10b781bf602a30510bacd2558e029c6871
Cedilhasemc <128654683+Cedilhasemc@users.noreply.github.com>
2023-10-31 22:16:01 -0300

    Update sketch_oct27a.ino
    Atualizando o código e adicionando leds e buzzer

--- sketch_oct27a.ino
+++ sketch_oct27a.ino
@@ -1,21 +1,29 @@
 #include <ArduinoJson.h>
 
-#define trigPin 6
-#define echoPin 7
+#define trigPin 10
+#define echoPin 9
+#define ledRedpin 5
+#define ledBluepin 4
+#define ledGreenpin 3
+#define buzzer 2
 
 int counter;
 float duration;
 float distance;
 unsigned long time;
 
-void setup() 
+void setup()
 {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(ledRedpin, OUTPUT);
+  pinMode(ledBluepin, OUTPUT);
+  pinMode(ledGreenpin, OUTPUT);
+  pinMode(buzzer, OUTPUT);
 }
 
-void loop() 
+void loop()
 {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -26,17 +34,17 @@ void loop()
 
   duration = 0;
   counter = 0;
-  while (--counter != 0) 
+  while (--counter != 0)
   {
-    if (PINB & 2) 
+    if (PINB & 2)
     {
       time = micros();
       break;
     }
   }
-  while (--counter != 0) 
+  while (--counter != 0)
   {
-    if ((PINB & 2) == 0) 
+    if ((PINB & 2) == 0)
     {
       duration = micros() - time;
       break;
@@ -47,22 +55,39 @@ void loop()
 
   StaticJsonDocument<100> json; // Tamanho do buffer para o JSON
 
-  if (distance < 100) 
+  if (distance < 10)
   {
-    json["distance"] = "Muito perto, pare agora";
-  } 
-  else if (distance >= 100 && distance <= 300) 
+    digitalWrite(5, HIGH);
+    digitalWrite(2, HIGH);
+    json["distance"] = distance; // Adicione a distância ao JSON
+    json["status"] = "Muito perto, pare agora";
+    delay(200);
+    digitalWrite(5, LOW);
+    digitalWrite(2, LOW );
+    delay(10);
+  }
+  else if (distance >= 10 && distance <= 30)
   {
-    json["distance"] = "Se aproximando, tome cuidado";
-  } 
-  else 
+    digitalWrite(4, HIGH);
+    json["distance"] = distance; // Adicione a distância ao JSON
+    json["status"] = "Se aproximando, tome cuidado";
+    delay(2000);
+    digitalWrite(4, LOW);
+    delay(10);
+  }
+  else
   {
-    json["distance"] = "Objeto Longe";
+    digitalWrite(3, HIGH);
+    json["distance"] = distance; // Adicione a distância ao JSON
+    json["status"] = "Objeto Longe";
+    delay(2000);
+    digitalWrite(3, LOW);
+    delay(10);
   }
 
   String jsonString;
   serializeJson(json, jsonString);
-  
+
   Serial.println(jsonString);
 
   delay(1000);
